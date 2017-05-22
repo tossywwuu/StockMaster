11296a7b7e6069017f0f768b23b69e9b2d32c35d
武文彬 <wuwb07@centaline.com.cn>
2017-05-22 16:23:29 +0800

    提交修改

---
+++ hom.DataAccess/Biz/StockPull.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace hom.DataAccess.Biz
+{
+    public class StockPull
+    {
+        public static void PullCurrent(string Code)
+        {
+            string Url = "";
+            System.Net.WebRequest wReq = System.Net.WebRequest.Create(Url);
+            // Get the response instance.
+            System.Net.WebResponse wResp = wReq.GetResponse();
+            System.IO.Stream respStream = wResp.GetResponseStream();
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(respStream, Encoding.GetEncoding("UTF-8")))
+            {
+                 reader.ReadToEnd();
+            }
+        }
+        public static void PullHistory(string Code)
+        {
+
+        }
+    }
+}
---
+++ hom.Web/Scripts/validate.js
@@ -0,0 +1,21 @@
+﻿function myvalidator() {
+    var valfuncs =
+        {
+            "isnull": function (value) {
+                if (value === "")
+                {
+                    alert(1);
+                }
+            }
+        }
+    var reg = function (name,func) { valfuncs[name]=func }
+     validator=function(flag,value) {
+        if (valfuncs[flag])
+        {
+            valfuncs[flag]();
+        }
+    }
+     return validator;
+}
+var valid = myvalidator();
+valid("isnull","");
